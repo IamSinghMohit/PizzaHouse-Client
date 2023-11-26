920df71c332fb0531830d3e39f51c710e0621c96
MohitNishad <nishadamar989@gmail.com>
2023-11-26 15:59:58 +0530

    create homepage making typography responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
@@ -5,16 +6,16 @@ export default function Home() {
     return (
         <div>
             <Navbar />
-            <div className="eclipse absolute -top-5 -left-[200px]"></div>
-            <div>
-                <div className="text-primary_black font-inter">
-                    <h1 className="text-[80px] leading-[110px]">
+            <div className="eclipse"></div>
+            <div className="flex flex-col gap-2">
+                <div className="text-primary_black font-inter flex flex-col ">
+                    <h1 className="text-[40px] leading-10">
                         Don't able to controll hunger{" "}
                         <span className="text-primary_orange">
                             order some pizza.
                         </span>
                     </h1>
-                    <p className="text-primary_gray first-letter font-[16px]">
+                    <p className="text-primary_gray first-letter text-[16px]">
                         Indulge in the ultimate pizza experience with our
                         mouthwatering selection that promises to elevate your
                         taste buds to new heights. Crafted with passion and
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,51 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+} from "./ui/dropdown-menu";
 interface Props {}
 
 export default function Navbar({}: Props) {
     return (
-        <nav className="navbar flex items-center justify-between sticky top-0 p-2">
-            <Image src="/logo.svg" alt="logo image" width={200} height={400} />
-            <div className="flex items-center font-bold font-inter">
-                <Link href={"/products"} className="hover:text-primary_orange">
-                    Products
-                </Link>{" "}
-                <Link href={"/"} className="hover:text-primary_orange">
-                    Home
-                </Link>
-                <Avatar>
-                    <AvatarImage src="lskdjf" alt="@shadcn" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-            </div>
+        <nav className="navbar sticky top-0">
+            <MaxWidthWrapper className="flex items-center justify-between py-2">
+                <div className="w-[120px] h-9 relative">
+                    <Image src="/logo.svg" alt="logo image" layout="fill" />
+                </div>
+                {/* <div className="flex items-center font-bold font-inter gap-2">
+                    <Link
+                        href={"/products"}
+                        className="hover:text-primary_orange"
+                    >
+                        Products
+                    </Link>{" "}
+                    <Link href={"/"} className="hover:text-primary_orange">
+                        Home
+                    </Link>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <Avatar>
+                                <AvatarImage src="lskdjf" alt="@shadcn" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <DropdownMenuContent>
+                                <DropdownMenuItem>
+                                    <span>Profile</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <span>Logout</span>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenuTrigger>
+                    </DropdownMenu>
+                </div> */}
+            </MaxWidthWrapper>
         </nav>
     );
 }
