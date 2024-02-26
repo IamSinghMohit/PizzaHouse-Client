7e9d25a239456c01c8f1adc4fe6bd17b9f8086e0
SinghMohit <nishadamar989@gmail.com>
2024-02-27 00:21:09 +0530

    fix socket.io for order page

--- src/app/order/[id]/components/ServerOrderRenderer.tsx
+++ src/app/order/[id]/components/ServerOrderRenderer.tsx
@@ -1,18 +1,39 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CImage from "@/lib/CImage";
 import { Card } from "@/components/ui/card";
 import OrderTopingRenderer from "./OrderTopingRenderer";
 import OrderSummary from "./OrderSummary";
-import {  useGetOrder } from "../hooks";
+import { useGetOrder } from "../hooks";
 import OrderStepper from "./OrderStepper";
 import OrderLoader from "./OrderLoader";
+import { OrderStatusEnum } from "../types";
+import { useSocket } from "@/app/socket-context";
 
-type Props = { id: string; };
+type Props = { id: string };
 
 function ServerOrderRenderer({ id }: Props) {
     const { data, isLoading } = useGetOrder(id);
+    const [step, setStep] = useState(OrderStatusEnum.PLACED);
+    const socket = useSocket()
+
+    useEffect(() => {
+        console.log(socket)
+        socket.current?.emit('join_room',id)
+        socket.current?.on("status_updated",(data) => setStep(data.status))
+        return () => {
+            socket.current?.emit("leave_room",id)
+            socket.current?.off("status_updated")
+        }
+    },[socket.current])
+
+    useEffect(() => {
+        if (data) {
+            setStep(data.status);
+        }
+    }, [data]);
+
     return !isLoading ? (
         <>
             <div className="flex flex-wrap gap-2 bg-gray-50 p-2 rounded-md border mb-2">
@@ -31,7 +52,7 @@ function ServerOrderRenderer({ id }: Props) {
             </div>
             <OrderTopingRenderer topings={data?.topings || []} />
             <Card className="p-2 mt-4 flex flex-col gap-4">
-                <OrderStepper step={step ? step : data?.status || "placed"} />
+                <OrderStepper step={step} />
                 <OrderSummary
                     price={data?.price || 0}
                     quantity={data?.quantity || 0}
