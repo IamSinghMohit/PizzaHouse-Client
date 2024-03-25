ae7598200d8d8dfc1d6b188ac19c3b307d554b4e
SinghMohit <nishadamar989@gmail.com>
2024-03-25 08:24:01 +0530

    add locomotive-scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,55 +8,61 @@ import ProductFeed from "./components/product-feed";
 import { Suspense } from "react";
 import ProductFeedLoader from "./components/product-feed/ProductFeedLoader";
 import ErrorBoundary from "@/lib/error-boundary";
+import { SmoothScrollProvider } from "@/components/SmoothScrollProvider";
 
 export default function Home() {
     return (
-        <main >
-            <HydrationProvider>
-                <MaxWidthWrapper className="md:px-2 lg:px-20">
-                    <div className="flex flex-col gap-1 sm:flex-row pt-5 ">
-                        <div className="text-primary_black font-inter flex flex-col p-2 gap-4 font-bold  max-w-[457px] items-center justify-center">
-                            <h1 className="text-[35px] leading-10 xs:text-[53px] xs:leading-[50px] lg:text-[70px] lg:leading-[72px]">
-                                Not able to controll your hunger{" "}
-                                <span className="text-primary_orange whitespace-nowrap">
-                                    order some
-                                    <Client>
-                                        <HomeTypeWriter />
-                                    </Client>
-                                    <Server>
-                                        <br />|
-                                    </Server>
-                                </span>
-                            </h1>
-                            <p className="text-primary_gray first-letter text-[12px] xs:text-[14px] md:text-[16px]">
-                                Indulge in the ultimate pizza experience with
-                                our mouthwatering selection that promises to
-                                elevate your taste buds to new heights.
-                            </p>
+        <main>
+            <SmoothScrollProvider
+                options={{ smooth: true, lenisOptions: { smoothTouch: true } }}
+            >
+                <HydrationProvider>
+                    <MaxWidthWrapper className="md:px-2 lg:px-20">
+                        <div className="flex flex-col gap-1 sm:flex-row pt-5 ">
+                            <div className="text-primary_black font-inter flex flex-col p-2 gap-4 font-bold  max-w-[457px] items-center justify-center">
+                                <h1 className="text-[35px] leading-10 xs:text-[53px] xs:leading-[50px] lg:text-[70px] lg:leading-[72px]">
+                                    Not able to controll your hunger{" "}
+                                    <span className="text-primary_orange whitespace-nowrap">
+                                        order some
+                                        <Client>
+                                            <HomeTypeWriter />
+                                        </Client>
+                                        <Server>
+                                            <br />|
+                                        </Server>
+                                    </span>
+                                </h1>
+                                <p className="text-primary_gray first-letter text-[12px] xs:text-[14px] md:text-[16px]">
+                                    Indulge in the ultimate pizza experience
+                                    with our mouthwatering selection that
+                                    promises to elevate your taste buds to new
+                                    heights.
+                                </p>
+                            </div>
+                            <HeroImage />
                         </div>
-                        <HeroImage />
-                    </div>
-                </MaxWidthWrapper>
-
-                <div className="home-clip w-full h-8 bg-primary_orange"></div>
-                <section className="bg-secondary_dark overflow-x-scroll hideScrollbar">
-                    <MaxWidthWrapper className="flex gap-4">
-                        {SectionCards.map((card) => (
-                            <ServiceCard
-                                key={card.id}
-                                content={card.content}
-                                image={card.image}
-                                heading={card.heading}
-                            />
-                        ))}
                     </MaxWidthWrapper>
-                </section>
-                <ErrorBoundary message="*Error while fetching products">
-                    <Suspense fallback={<ProductFeedLoader />}>
-                        <ProductFeed />
-                    </Suspense>
-                </ErrorBoundary>
-            </HydrationProvider>
+
+                    <div className="home-clip w-full h-8 bg-primary_orange"></div>
+                    <section className="bg-secondary_dark overflow-x-scroll hideScrollbar">
+                        <MaxWidthWrapper className="flex gap-4">
+                            {SectionCards.map((card) => (
+                                <ServiceCard
+                                    key={card.id}
+                                    content={card.content}
+                                    image={card.image}
+                                    heading={card.heading}
+                                />
+                            ))}
+                        </MaxWidthWrapper>
+                    </section>
+                    <ErrorBoundary message="*Error while fetching products">
+                        <Suspense fallback={<ProductFeedLoader />}>
+                            <ProductFeed />
+                        </Suspense>
+                    </ErrorBoundary>
+                </HydrationProvider>
+            </SmoothScrollProvider>
         </main>
     );
 }
